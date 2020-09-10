131d945e42ad6d194b76d4f027028e2159a6d140
andrewmundung <s21810094@student.unklab.ac.id>
2020-09-10 01:06:07 -0700

    second commit

---
+++ pertemuan_3/index.js
@@ -0,0 +1,12 @@
+const firstName = "andrew"; 
+const lastName = "mundung";
+let age = 20 ;  
+let status = "single";
+
+console.log(firstName); 
+console.log(lastName);
+console.log(age);
+console.log(status);
+
+const info = " nama saya adalah " + firstName + lastName + " umur saya " + age + " tahun, status " + status ; //array dapat menampung banyak tipe data   
+console.log(info);
---
+++ pertemuan_4/index.js
@@ -0,0 +1,6 @@
+let a = 10;
+let b = 4;
+let LuasA = a * a;
+let LuasB = b * b;
+let total = LuasA + LuasB;
+console.log(total);
