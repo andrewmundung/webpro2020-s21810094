50b38130a9a27f237837b0cd714b5b53af2a2a0d
andrewmundung <s21810094@student.unklab.ac.id>
2020-11-11 22:16:51 -0800

    pertemuan terbaru tindih

---
+++ pertemuan_20/src/Utils/Person.js
@@ -0,0 +1,26 @@
+export const person = [
+    {
+        name: "amelia",
+        institution: "Unklab",
+        address: "Manado",
+        phone: "085xxxxxxx",
+    },
+    {
+        name: "calliope",
+        institution: "Unima",
+        address: "Manado",
+        phone: "082xxxxxxx",
+    },
+    {
+        name: "coco",
+        institution: "Unsrat",
+        address: "Manado",
+        phone: "089xxxxxxx",
+    },
+    {
+        name: "hehe",
+        institution: "Unklab",
+        address: "Manado",
+        phone: "081xxxxxxx",
+    },
+    ];
